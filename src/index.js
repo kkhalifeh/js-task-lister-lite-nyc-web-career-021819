e6697c2c8cf766e9ee430dc161c3424126719cd1
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-04-03 00:13:57 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const mainForm = document.querySelector('#create-task-form');
+  const textField = document.querySelector('#new-task-description');
+  const createButton = document.querySelector('#new-task-button')
+  const tasks = document.querySelector('#tasks')
+
+  mainForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    // console.log('e', e.value);
+    const textField = document.querySelector('#new-task-description');
+    console.log('textField.value', textField.value);
+    // tasks.innerHTML = '<li>'+textField.value+'</li>';
+    var task = document.createElement('li')
+     tasks.appendChild(task)
+     task.innerText = `${textField.value}`
+
+     // Create a button
+     const h1 = document.querySelector('#lists-h1');
+     var newButton =  document.createElement('button');
+     newButton.innerText = 'X'
+     newButton.id = `${textField.value}`
+     newButton.setAttribute('data-description', `${textField.value}`);
+     console.log('newButton', newButton);
+     tasks.insertBefore(newButton, h1)
+
+     newButton.addEventListener('click', function (e) {
+       console.log('task', task);
+       task.remove();
+       newButton.remove();
+     });
+  });
 });
